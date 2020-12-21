26521a34a8de380c9c1ee62c71f052985603ebf8
l00pinfinity <loopinfinity@criptext.com>
2020-12-21 14:29:16 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-$('.design').click(function () {
-    $(this).children('.hide').slideToggle(1300);
-    $(this).children('.show').slidetoggle(1400);
-  });
+$("#design  ").click(function(){
+  $(this).children(".hide").slideToggle(1300);
+  $(this).children(".show").slideToggle(1400);
+});
+$(".card-1").hover(function(){
+  $(this).children(".card-1").fadeToggle(500);
+});
