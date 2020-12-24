bd1ab505dbe7ea5af504b2e945fa89f44a134033
l00pinfinity <loopinfinity@criptext.com>
2020-12-24 12:24:45 +0300

    add alert info in the form

--- js/script.js
+++ js/script.js
@@ -10,3 +10,9 @@ $('.design').click(function () {
 $(".card").hover(function () {
  $(this).children(".card-1").fadeToggle(500);
 });
+
+
+document.querySelector('form').onsubmit = function () {
+  const name = document.querySelector('#info_name').value;
+  alert(`Hello, ${name} we have received your message. Thank you for reaching out to us.!`);
+};
