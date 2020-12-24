36fbf0af82d747727ac4ff6e889a02be3bc4c8c8
l00pinfinity <loopinfinity@criptext.com>
2020-12-24 16:42:24 +0300

    disable submit button until name is put

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+// Hide and show the whole what we do part
+
 // $(document).ready(function(){
 //   $(".show").click(function(){
 //     $(".hide").show();
 //   });
 // });
+
 $('.design').click(function () {
  $(this).children('.hide').slideToggle(1300);
  $(this).children('.show').slidetoggle(1400);
@@ -11,8 +14,16 @@ $(".card").hover(function () {
  $(this).children(".card-1").fadeToggle(500);
 });
 
+document.addEventListener('DOMContentLoaded', function () {
 
-document.querySelector('form').onsubmit = function () {
-  const name = document.querySelector('#info_name').value;
-  alert(`Hello, ${name} we have received your message. Thank you for reaching out to us.!`);
-};
+  //By default the submit button is disabled
+  document.querySelector('#submit').disabled = true;
+
+  document.querySelector('#info_name').onkeyup = () => {
+    document.querySelector('#submit').disabled = false;
+  }
+  document.querySelector('form').onsubmit = function () {
+    const name = document.querySelector('#info_name').value;
+    alert(`Hello, ${name} we have received your message. Thank you for reaching out to us.`)
+  };
+});
